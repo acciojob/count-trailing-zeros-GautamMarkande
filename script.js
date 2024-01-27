aba514e0a3794900f7f28f0b444428b9c0cfa57d
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-27 19:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let divider = 5;
+	let count = 0;
+	while (parseInt(n/divider)>=1) {
+		count = count + parseInt(n/divider);
+		divider = divider*5;
+	}
+	return count;
 }
 
 const input = prompt("Enter a number");
